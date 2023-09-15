23521dababf55edcd60eb34a3dc8807a6f013bdc
DaniSodeiro <danielasodeirosilva@gmail.com>
2023-09-14 21:51:25 -0300

    :sparkles: feat: Adiciona atributo ID a entidade especialidade

--- src/database/models/especialidade.js
+++ src/database/models/especialidade.js
@@ -4,6 +4,14 @@ import { Sequelize, DataTypes } from "sequelize";
 const sequelize = new Sequelize("sqlite::memory:");
 
 const Especialidade = sequelize.define("Especialidade", {
+      // Um identificador único para cada usuário registrado na plataforma.
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true, // Permite que o valor seja gerado automaticamente
+    unique: true,
+    allowNull: false,
+  },
   nome: {
     type: DataTypes.STRING,
     allowNull: false,
