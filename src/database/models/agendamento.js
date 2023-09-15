0dc0a009249e54b9d7dbd0bfd5b2af2b5058a006
Taw Simões <tawm.dev@gmail.com>
2023-09-15 15:15:58 -0300

    ":bulb: docs: Adiciona comentários com sugetões de melhorias para o Model agendamento.js"

--- src/database/models/agendamento.js
+++ src/database/models/agendamento.js
@@ -12,25 +12,31 @@ import { Sequelize, DataTypes } from "sequelize";
 // Cria uma instância do Sequelize e especifica o banco de dados SQLite.
 const sequelize = new Sequelize("sqlite::memory:");
 
-
 // Entidade "agendamento"
 const Agendamento = sequelize.define("Agendamento", {
+  // Retirar data
   data: {
     type: DataTypes.DATE,
     allowNull: false,
   },
+  // Retirar horário
   horário: {
     type: DataTypes.TIME,
     allowNull: false,
   },
+  // Retirar local
   local: {
     type: DataTypes.STRING,
     allowNull: false,
   },
+  // Manter esse atributo, mas trocar o nome dele para Mensagem.
   assunto: {
     type: DataTypes.STRING,
     allowNull: false,
   },
+  // Retirar esse atributo, pois eles estará associado via 'Associações' feita mais abaixo.
+  // Nesse caso foi feita uma referência, mas o ideal é associação para que no service seja possível obter o mentor relacionado ao agendamento.
+  // Link sobre esse assunto: https://stackoverflow.com/questions/46305459/sequelize-model-references-vs-associations
   mentor: {
     type: DataTypes.INTEGER,
     allowNull: false,
@@ -39,6 +45,9 @@ const Agendamento = sequelize.define("Agendamento", {
       key: "id",
     },
   },
+  // Retirar esse atributo, pois eles estará associado via 'Associações' feita mais abaixo.
+  // Nesse caso foi feita uma referência, mas o ideal é associação para que no service seja possível obter o mentor relacionado ao agendamento.
+  // Link sobre esse assunto: https://stackoverflow.com/questions/46305459/sequelize-model-references-vs-associations
   mentorado: {
     type: DataTypes.INTEGER,
     allowNull: false,
@@ -49,7 +58,10 @@ const Agendamento = sequelize.define("Agendamento", {
   },
 });
 
-// Associações 
+// Associações
+// Mater a associação no lugar da referência acima.
+// Trocar a palavra sessão para agendamento.
+// Não podemos colocar palavras com caracteres especiais nessa parte, exemplo: sessão.
 Sessão.belongsTo(Mentor, {
   foreignKey: {
     allowNull: false,
@@ -63,4 +75,5 @@ Sessão.belongsTo(Mentorado, {
 });
 
 // Exporta as entidades
+// Aqui deve exportar apenas o agendamento sem {} e mentor e mentorado.
 export { Mentor, Mentorado, Agendamento };
