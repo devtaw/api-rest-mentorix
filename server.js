ed13d8a28b9a3ac25bf0c7d1bcda7d0dd186dc04
Taw Simões <tawm.dev@gmail.com>
2023-09-15 11:58:47 -0300

    :sparkles: feat: Define roteamento para Autenticação e Área

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 //Importação do express
 import express from "express";
+import { AutenticacaoController } from "./src/controllers/autenticacao-controller.js";
+import { AreaController } from "./src/controllers/area-controller.js";
 
 //constante para rodar o express
 const app = express();
 
-//Const para definir o port 
+//Const para definir o port
 const port = process.env.PORT || 3000;
 
 //Middleware para reconhecimento do formato JSON
@@ -13,8 +15,10 @@ app.use(express.json());
 //Aqui, importa-se o controller;
 
 //Aqui, chama-se as rotas
+app.use("/autenticacao", AutenticacaoController);
+app.use("/area", AreaController);
 
-//Aqui, é definido o "levante" para o servidor da API 
+//Aqui, é definido o "levante" para o servidor da API
 app.listen(port, () => {
-    console.log(`Servidor rodando na porta http://localhost${port}`)
+  console.log(`Servidor rodando na porta http://localhost${port}`);
 });
