3fd941ebf0aaacd372c4bc3a663da2641bdce669
Jéssica Hickembick <j.hickembick@gmail.com>
2023-09-16 19:21:00 -0300

    :sparkles: feat: Adiciona rota GET para Mentorado

---
+++ src/controllers/mentorado-controller.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import Mentorado from '../database/models/mentorado.js';
+
+const routes = express.Router();
+
+routes.get('/', async (req, res) => {
+    try {
+      const listaMentorados = await Mentorado.findAll();
+      return res.status(200).json(listaMentorados);
+    } catch (error) {
+      return res.status(500).json({ error: 'Erro ao listar mentorados.' });
+    }
+  });
+
+  routes.get('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const mentorado = await Mentorado.findByPk(id);
+
+    if (!mentorado) {
+      return res.status(404).json({ error: 'Usuario não encontrado.' });
+    }
+
+    return res.status(200).json(mentorado);
+  } catch (error) {
+    return res.status(500).json({ error: 'Erro ao buscar mentorado.' });
+  }
+});
