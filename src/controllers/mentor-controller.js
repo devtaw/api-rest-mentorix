5b7695d728b4b56d5922fb306be89a6c787811da
Hellraiser997 <felipesg03@gmail.com>
2023-09-15 23:12:06 -0300

    :sparkles: Feat: criada função para listar mentores no mentor-controller.js

--- src/controllers/mentor-controller.js
+++ src/controllers/mentor-controller.js
@@ -1,2 +1,11 @@
 import Mentor from "../database/models/mentor.js";
 
+async function listarMentores(req, res) {
+    try {
+        const mentores = await Mentor.findAll();
+        res.json(mentores);
+    } catch (error) {
+        res.status(500).json({error: 'Erro ao listar mentores.'});
+    };
+};
+
