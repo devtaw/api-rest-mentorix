ca0566bf7c359580eb607ec8ebd0552d76c2fe4c
Hellraiser997 <felipesg03@gmail.com>
2023-09-16 09:26:36 -0300

    :sparkles: Feat: criada rota get em especialidade-controller

--- src/controllers/especialidade-controller.js
+++ src/controllers/especialidade-controller.js
@@ -1,3 +1,15 @@
 import express from 'express';
 const routes = express.Router;
 
+routes.get ("/", (req, res) => {
+    try {
+        console.log("get especialide");
+        const listaEspecialide = [];
+        return response.status(200).json(listaEspecialide);
+    } catch (error) {
+        return response.status(500).json({
+            message: "Erro ao listar especialidades"
+        });
+    }
+});
+
