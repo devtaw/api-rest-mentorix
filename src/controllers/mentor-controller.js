c254c0df982972eda6c4e9f2dafd4bbd824bd11b
Hellraiser997 <felipesg03@gmail.com>
2023-09-15 23:23:24 -0300

    :sparkles: Feat: criada função de criar mentor no mentor-controller.js

--- src/controllers/mentor-controller.js
+++ src/controllers/mentor-controller.js
@@ -6,6 +6,16 @@ async function listarMentores(req, res) {
         res.json(mentores);
     } catch (error) {
         res.status(500).json({error: 'Erro ao listar mentores.'});
-    };
+    }
+};
+
+async function criarMentor(req, res) {
+    const {nome, especialidade} = req.body;
+    try {
+        const mentor = await Mentor.create({nome, especialidade});
+        res.status(201).json(mentor);
+    } catch (error) {
+        res.status(500).json({error: 'Erro ao criar mentor.'});
+    }
 };
 
