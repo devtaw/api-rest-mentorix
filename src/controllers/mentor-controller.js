15bca629b9c2c2db9919501a69158bc736df6c0a
Hellraiser997 <felipesg03@gmail.com>
2023-09-15 23:43:42 -0300

    :boom: Fix: removendo funções e adicionando rotas no mentor-controller.js

--- src/controllers/mentor-controller.js
+++ src/controllers/mentor-controller.js
@@ -1,21 +1,12 @@
 import Mentor from "../database/models/mentor.js";
+const routes = express.Router();
 
-async function listarMentores(req, res) {
+routes.get ("/", async (req, res) => {
     try {
-        const mentores = await Mentor.findAll();
-        res.json(mentores);
+        console.log("get Mentor");
+        const listaMentores = [];
+        return response.status(200).json(listaMentores);
     } catch (error) {
-        res.status(500).json({error: 'Erro ao listar mentores.'});
+        return response.status(500).json({error: 'Erro ao listar mentores.'});
     }
-};
-
-async function criarMentor(req, res) {
-    const {nome, especialidade} = req.body;
-    try {
-        const mentor = await Mentor.create({nome, especialidade});
-        res.status(201).json(mentor);
-    } catch (error) {
-        res.status(500).json({error: 'Erro ao criar mentor.'});
-    }
-};
-
+});
