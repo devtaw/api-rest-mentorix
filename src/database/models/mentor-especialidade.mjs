7e541f3a7392adec9fc66cdb4b4ec78053147f32
DaniSodeiro <danielasodeirosilva@gmail.com>
2023-09-17 16:10:56 -0300

    :sparkles: feat: Adiciona entidade Mentor Especialidade

---
+++ src/database/models/mentor-especialidade.mjs
@@ -0,0 +1,32 @@
+// Importa o objeto DB, que parece ser um objeto relacionado ao Sequelize.
+import DB from "./index.cjs";
+const sequelize = DB.sequelize;
+const { DataTypes } = DB.Sequelize;
+
+// Define o modelo "MentorEspecialidade" para representar a associação entre Mentores e Especialidades.
+const MentorEspecialidade = sequelize.define(
+  "MentorEspecialidade",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      unique: true,
+      allowNull: false,
+    },
+    mentorID: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    especialidadeID: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  },
+  {
+    // Define o nome da tabela no banco de dados como "mentorepecialidade"
+    tableName: "mentorepecialidade",
+  }
+);
+
+export default MentorEspecialidade;
