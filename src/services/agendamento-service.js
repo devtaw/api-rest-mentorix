9cf9f1d61781651f5f9235eb8904bef72e696e0d
Hellraiser997 <felipesg03@gmail.com>
2023-09-17 20:51:09 -0300

    :sparkles: Feat: adicionado código no arquivo agendamento-service.js

--- src/services/agendamento-service.js
+++ src/services/agendamento-service.js
@@ -1,1 +1,32 @@
 // Onde será implementado o CRUD.
+import Agendamento from "../database/models/agendamento.mjs";
+export class AgendamentoService {
+    async getAllAgendamentos() {
+        return AgendamentoModel.findAll();
+    }
+    async getAgendamentosById(idAgendamento) {
+        return AgendamentoModel.findByPk(idAgendamento);
+    }
+    async addAgendamento(dadosAgendamento) {
+        return AgendamentoModel.create(dadosAgendamento);
+    }
+    async updateAgendamento(idAgendamento, dadosAgendamento) {
+        const agendamento = await Agendamento.Model.findByPk(idAgendamento);
+
+        // if (!especialidade) {
+        //   throw new Error("Especialidade não encontrada.");
+        // }
+
+        return agendamento.update(dadosAgendamento);
+    }
+
+    async deleteAgendamento(AgendamentoId) {
+        const agendamento = await AgendamentoModel.findByPk(AgendamentoId);
+
+          // if (!especialidade) {
+        //   throw new Error("Especialidade não encontrada.");
+        // }
+
+        return agendamento.destroy();
+    }
+}
