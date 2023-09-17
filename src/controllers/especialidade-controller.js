8701733512118275803fb6a8a598e2c495ffa75c
Hellraiser997 <felipesg03@gmail.com>
2023-09-17 10:16:23 -0300

    :bug: Fix: alteração de nome const "especialide" para "especialidade" no especialidade-controller

--- src/controllers/especialidade-controller.js
+++ src/controllers/especialidade-controller.js
@@ -3,9 +3,9 @@ const routes = express.Router;
 
 routes.get ("/", (req, res) => {
     try {
-        console.log("get especialide");
-        const listaEspecialide = [];
-        return response.status(200).json(listaEspecialide);
+        console.log("get especialidade");
+        const listaEspecialidade = [];
+        return response.status(200).json(listaEspecialidade);
     } catch (error) {
         return response.status(500).json({
             message: "Erro ao listar especialidades"
