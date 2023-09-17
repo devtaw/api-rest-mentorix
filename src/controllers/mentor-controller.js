3ed7e8cb506bec5e1103e7b7612c0b91f01c44bf
Hellraiser997 <felipesg03@gmail.com>
2023-09-17 10:57:46 -0300

    :bug: Fix: padronização de parâmetros req e res em mentor-controller

--- src/controllers/mentor-controller.js
+++ src/controllers/mentor-controller.js
@@ -5,9 +5,9 @@ routes.get ("/", async (req, res) => {
     try {
         console.log("get Mentor");
         const listaMentores = [];
-        return response.status(200).json(listaMentores);
+        return res.status(200).json(listaMentores);
     } catch (error) {
-        return response.status(500).json({error: 'Erro ao listar mentores.'});
+        return res.status(500).json({error: 'Erro ao listar mentores.'});
     }
 });
 
@@ -15,11 +15,11 @@ routes.get ("/:id", (req, res) => {
     try {
         const body = req.body; 
         const idMentor = req.params.id;
-        return response.status(200).json({
+        return res.status(200).json({
             message: "caiu no endpoint get mentor by id" + idMentor
         })
     } catch (error) {
-        return response.status(500).json({
+        return res.status(500).json({
             error: "Erro ao listar mentor"
         });
     }
@@ -27,38 +27,38 @@ routes.get ("/:id", (req, res) => {
 
 routes.post ("/", (req, res) => {
     try{
-        const body = request.body;
+        const body = req.body;
         console.log("post mentor")
-        return response.status(200).json({
+        return res.status(200).json({
             message: 'caiu no endpoint post mentor',
             body,
         });
     } catch (error) {
-        return response.status(500).json({error: 'Erro ao cadastrar mentor'});
+        return res.status(500).json({error: 'Erro ao cadastrar mentor'});
     }
 });
 
 routes.put ("/:id", (req, res) => {
     try {
-        const body= request.body;
+        const body= req.body;
         const idMentor = req.params.id;
-        return response.status(200).json({
+        return res.status(200).json({
             message: 'Caiu no endpoint put mentor by id' + idMentor,
             body,
         });
     } catch (error) {
-        return response.status(500).json({error: 'Erro ao incluir mentor'});
+        return res.status(500).json({error: 'Erro ao incluir mentor'});
     }
 });
 
 routes.delete("/:id", (req, res) => {
     try {
         const idMentor = req.params.id;
-        return response.status(200).json({
+        return res.status(200).json({
             message: 'Caiu no endpoint delete by id' + idMentor
         })
     } catch (error) {
-        return response.status(500).json({
+        return res.status(500).json({
             error: 'erro ao deletar mentor'
         });
     }
