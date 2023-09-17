6b61ac0004b502cfe2fe32b3e74ab2d0d17dcbc0
DaniSodeiro <danielasodeirosilva@gmail.com>
2023-09-17 01:37:05 -0300

    :sparkles: feat: Adiciona o service do User

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,10 +1,41 @@
-
-// Método para verificar a senha
+import UserModel from "../database/models/user.mjs"
 export class UserService {
-verificarSenha(senha) {
-return bcrypt.compareSync(senha, this.senhaCriptografada);
+
+  async getAllUsers() {
+    return UserModel.findAll();
+  }
+
+  async getUserById(idUser) {
+    return UserModel.findByPk(idUser);
+  }
+
+  async addUser(dadosUser) {
+    return UserModel.create(dadosUser);
+  }
+
+  async updateUser(idUser, dadosUser) {
+    const user = await UserModel.findByPk(idUser);
+    
+    await user.update(dadosUser);
+  }
+
+  async deleteUser(idUser) {
+    const user = await UserModel.findByPk(idUser);
+
+    // if (!user) {
+    //   return res.status(404).json({ error: "Usuário não encontrado." });
+    // }
+
+    return user.destroy();
+  }
 }
-}
+
+// // Método para verificar a senha
+// export class UserService {
+// verificarSenha(senha) {
+// return bcrypt.compareSync(senha, this.senhaCriptografada);
+// }
+// }
 
 
 
