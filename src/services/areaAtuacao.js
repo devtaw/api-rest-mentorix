80a70375e05588a65766fafbba0c8e8a59b9256f
DaniSodeiro <danielasodeirosilva@gmail.com>
2023-09-18 00:44:55 -0300

    :sparkles: feat: Adiciona Entidade no service areaAtuacao.

---
+++ src/services/areaAtuacao.js
@@ -0,0 +1,11 @@
+import AreaAtuacaoModel from "../database/models/area-atuacao.js";
+
+export class AreaAtuacaoService {
+  async getAllAreas() {
+    return await AreaAtuacaoModel.findAll();
+  }
+
+  async addArea(newArea) {
+    return AreaAtuacaoModel.create(newArea);
+}
+}
