e6f1e1828df7a0e70ce4717b2610d359c2200068
DaniSodeiro <danielasodeirosilva@gmail.com>
2023-09-18 22:41:39 -0300

    :sparkles: feat: Adiciona validações da User-Service

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -6,17 +6,29 @@ export class UserService {
   }
 
   async getUserById(idUser) {
-    return UserModel.findByPk(idUser);
+    const user = UserModel.findAll(idUser);
+    if (!user) {
+      throw new ServiceError("User não encontrada", 404);
+    }
+    return User;
   }
 
   async addUser(dadosUser) {
+    if (!dadosUser.email || !dadosUser.email.trim()) {
+      throw new ServiceError("Email é obrigatório", 400);
+    }
+
     return UserModel.create(dadosUser);
   }
 
   async updateUser(idUser, dadosUser) {
     const user = await UserModel.findByPk(idUser);
     if (!user) {
-      throw ServiceError("Usuário não encontrado", 404);
+      throw new ServiceError("User não encontrada", 404);
+    }
+
+    if (!dadosUser.senha || !dadosUser.senha.trim()) {
+      throw new ServiceError("Email é obrigatório", 400);
     }
 
     return user.update(dadosUser);
@@ -26,7 +38,7 @@ export class UserService {
     const user = await UserModel.findByPk(idUser);
 
     if (!user) {
-      throw ServiceError("Usuário não encontrado", 404);
+      throw new ServiceError("Usuário não encontrado", 404);
     }
 
     return user.destroy();
