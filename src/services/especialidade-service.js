4b1ffe1b7f32afac903f612b2dbd754581a0f9f4
DaniSodeiro <danielasodeirosilva@gmail.com>
2023-09-18 21:52:29 -0300

    :sparkles: feat: Adiciona validações da Especialidade -Service

--- src/services/especialidade-service.js
+++ src/services/especialidade-service.js
@@ -1,11 +1,18 @@
 import EspecialidadeModel from "../database/models/especialidade.mjs";
+import { ServiceError } from "../common/service-error.js";
+
 export class EspecialidadeService {
   async getAllEspecialidades() {
     return EspecialidadeModel.findAll();
   }
 
   async getEspecialidadeById(idEspecialidade) {
-    return EspecialidadeModel.findByPk(idEspecialidade);
+    const especialidade = EspecialidadeModel.findByPk(idEspecialidade);
+
+    if (!especialidade) {
+      throw new ServiceError("Especialidade não encontrada", 404);
+    }
+    return especialidade;
   }
 
   async addEspecialidade(dadosEspecialidade) {
@@ -16,7 +23,7 @@ export class EspecialidadeService {
     const especialidade = await EspecialidadeModel.findByPk(idEspecialidade);
 
     if (!especialidade) {
-      throw new Error("Especialidade não encontrada.");
+      throw new ServiceError("Especialidade não encontrada", 404);
     }
 
     return especialidade.update(dadosEspecialidade);
@@ -26,7 +33,7 @@ export class EspecialidadeService {
     const especialidade = await EspecialidadeModel.findByPk(especialidadeId);
 
     if (!especialidade) {
-      throw new Error("Especialidade não encontrada.");
+      throw new ServiceError("Especialidade não encontrada", 404);
     }
 
     return especialidade.destroy();
