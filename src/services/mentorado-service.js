19600da280c1ee7590b1ac8396979de626df4199
Hellraiser997 <felipesg03@gmail.com>
2023-09-18 22:00:10 -0300

    :sparkles: Feat: adicionada validação em deleteMentorado em mentorado-service.js

--- src/services/mentorado-service.js
+++ src/services/mentorado-service.js
@@ -17,13 +17,16 @@ export class MentoradoService {
     if (!mentorado) {
       // return res.status(404).json({ error: "Mentorado não encontrado." });
       // TODO: ACRESCENTAR EM TODOS OS CONTROLLERS QUE RETORNARM O SERVICE ERROR - THROW NEW LANÇA UM ERRO OU EXCEÇÃO E ELE PARA A EXECUAÇÃO DO ENDPOINT E LANÇA UM ERRO PARA QUEM ESTIVER CHAMANDO A FUNÇÃO
-      throw new ServiceError("Mentorado não encontrado.");
+      throw new ServiceError("Mentorado não encontrado.", 404);
     }
 
     await mentorado.update(dadosMentorado);
   }
   async deleteMentorado(idMentorado) {
     const mentorado = await MentoradoModel.findByPk(idMentorado);
+    if(!mentorado) {
+      throw new ServiceError("Mentorado não encontrado.", 404);
+    }
 
     // if (!mentorado) {
     //    return res.status(404).json({ error: 'Mentorado não encontrado.' });
