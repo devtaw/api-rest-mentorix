fb86b9207ec4fb7b52f33ecca9546072c815c73a
Taw Simões <tawm.dev@gmail.com>
2023-09-19 00:47:06 -0300

    refactor: ajuste na configuracao do sequelize

--- src/database/models/index.cjs
+++ src/database/models/index.cjs
@@ -6,16 +6,9 @@ const Sequelize = require("sequelize");
 const process = require("process");
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || "development";
-const config = require(__dirname + "/../config/config.json")[env];
 const db = {};
 
 let sequelize;
-
-// if (config.use_env_variable) {
-//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
-// } else {
-//   sequelize = new Sequelize(config.database, config.username, config.password, config);
-// }
 
 if (process.env.NODE_ENV == "production") {
   sequelize = new Sequelize(process.env.database, process.env.username, process.env.password, {
@@ -24,6 +17,7 @@ if (process.env.NODE_ENV == "production") {
     dialect: "mysql",
   });
 } else {
+  const config = require(__dirname + "/../config/config.json")[env];
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
