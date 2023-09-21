f0ef70b84524b187bb95b1303863a57f95bb2f1f
DaniSodeiro <danielasodeirosilva@gmail.com>
2023-09-20 21:30:00 -0300

    :boom: fix:  Ajuste na autenticacao -controller .

--- src/controllers/autenticacao-controller.js
+++ src/controllers/autenticacao-controller.js
@@ -1,16 +1,28 @@
-// CONVERSAR COM O LÉO SOBRE TER APENAS UM VERBO HTTP NO RECURSO AUTENTICAÇÃO (QUE SERVE PARA FAZER O LOGIN),
-// POIS NÃO COSTUMA SER NECESSÁRIO MAIS VERBOS PARA AUTENTICAR.
-
 import express from "express";
 import { UserService } from "../services/user-service.js";
+
 const routes = express.Router();
 const userService = new UserService();
 
 routes.post("/", (request, response) => {
-  console.log("get autenticacao");
-  return response.status(200).json({
-    message: "Caiu no endpoint post autenticacao",
-  });
+  try {
+    const login = request.body.login;
+    const senha = request.body.senha;
+
+    const usuarioAutenticado = userService.autenticar(login, senha);
+
+    return response.status(200).json(usuarioAutenticado);
+  } catch (error) {
+    console.error(error);
+
+    if (error instanceof ServiceError) {
+      return response.status(error.errorCode).json({ messagem: error.message });
+    }
+
+    return response
+      .status(500)
+      .json({ error: "Ocorreu um erro ao autenticar o usuário!" });
+  }
 });
 
 export const AutenticacaoController = routes;
