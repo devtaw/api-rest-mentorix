842fa7342124248c3bc3250b629398d1a10c8503
Taw Simões <tawm.dev@gmail.com>
2023-10-31 11:26:43 -0300

    :wrench: chore: Ajustes nas configurações de envio de e-mail

--- src/common/mail-client.js
+++ src/common/mail-client.js
@@ -9,6 +9,8 @@ const host = process.env.EMAIL_SMTP || configs?.EMAIL_SMTP;
 const port = process.env.EMAIL_PORT || configs?.EMAIL_PORT;
 const user = process.env.EMAIL_USER || configs?.EMAIL_USER;
 const password = process.env.EMAIL_PASSWORD || configs?.EMAIL_PASSWORD;
+const clientId = process.env.EMAIL_CLIENT_ID || configs?.EMAIL_CLIENT_ID;
+const clientSecret = process.env.EMAIL_CLIENT_SECRET || configs?.EMAIL_CLIENT_SECRET;
 
 // console.log("-------------------------------------------");
 // console.log(host);
@@ -24,6 +26,8 @@ export const mailClient = nodemailer.createTransport({
   auth: {
     user: user,
     pass: password,
+    clientId: clientId,
+    clientSecret: clientSecret,
   },
 });
 
