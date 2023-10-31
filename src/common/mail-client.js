df02dbb745949b5ed00914dd70e0f1d3f52db9dc
Taw Simões <tawm.dev@gmail.com>
2023-10-31 11:35:48 -0300

    :wrench: chore: Ajustes nas configurações de envio de e-mail

--- src/common/mail-client.js
+++ src/common/mail-client.js
@@ -12,12 +12,14 @@ const password = process.env.EMAIL_PASSWORD || configs?.EMAIL_PASSWORD;
 const clientId = process.env.EMAIL_CLIENT_ID || configs?.EMAIL_CLIENT_ID;
 const clientSecret = process.env.EMAIL_CLIENT_SECRET || configs?.EMAIL_CLIENT_SECRET;
 
-// console.log("-------------------------------------------");
-// console.log(host);
-// console.log(port);
-// console.log(user);
-// console.log(password);
-// console.log("-------------------------------------------");
+console.log("-------------------------------------------");
+console.log(host);
+console.log(port);
+console.log(user);
+console.log(password);
+console.log(clientId);
+console.log(clientSecret);
+console.log("-------------------------------------------");
 
 export const mailClient = nodemailer.createTransport({
   host: host,
@@ -26,6 +28,7 @@ export const mailClient = nodemailer.createTransport({
   auth: {
     user: user,
     pass: password,
+    password: password,
     clientId: clientId,
     clientSecret: clientSecret,
   },
